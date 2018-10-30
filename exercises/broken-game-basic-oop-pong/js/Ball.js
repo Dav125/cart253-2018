c575b60a3a7b3b7cca3ff2599802340714f5fcc5
David Fong <dav770@outlook.com>
2018-10-30 13:27:56 -0400

    Exercise 6: Ball.js: there was an extra + sign in the code

--- exercises/broken-game-basic-oop-pong/js/Ball.js
+++ exercises/broken-game-basic-oop-pong/js/Ball.js
@@ -44,9 +44,9 @@ Ball.prototype.updated = function () {
 Ball.prototype.isOffScreen = function () {
   // Check for going off screen and reset if so
 
-  ///////////////// Fix 
+  ///////////////// Fix
   // typo error with iff, we are supposed to write "if"
-  if (this.x ++ this.size < 0 && this.x > width) {
+  if (this.x + this.size < 0 && this.x > width) {
     return true;
   }
   else {
