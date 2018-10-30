11893b0c4fba4d40e5b657b1ce6b77faafed0466
David Fong <dav770@outlook.com>
2018-10-30 17:16:04 -0400

    Exercise 6: script.js: lowered the velocity of the ball

--- exercises/broken-game-basic-oop-pong/js/script.js
+++ exercises/broken-game-basic-oop-pong/js/script.js
@@ -26,7 +26,9 @@ function setup() {
   createCanvas(640,480);
   noStroke();
   // Create a ball
-  ball = new Ball(width/2,height/2,50,50,10,50);
+/////////////////////////////////////////////////////// FIX
+// lower velocity of the ball
+  ball = new Ball(width/2,height/2,50,50,10,5);
   // Create the right paddle with UP and DOWN as controls
 
   ///////////////////////////////////////////////////////// FIX
