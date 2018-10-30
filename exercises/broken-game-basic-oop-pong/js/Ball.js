a8224173f837c35d0d67419dce80a5242a8cbc4b
David Fong <dav770@outlook.com>
2018-10-30 13:16:02 -0400

    Exercise 6: Ball.js: typo with prototype and one extra unnessary parenthese
    Typo with prototyp and rewrote prototype and there was an extra parenthese which I erased it.

--- exercises/broken-game-basic-oop-pong/js/Ball.js
+++ exercises/broken-game-basic-oop-pong/js/Ball.js
@@ -62,7 +62,11 @@ Ball.prototype.display = function () {
 //
 // Check if this ball overlaps the paddle passed as an argument
 // and if so reverse x velocity to bounce
-Ball.prototyp.handleCollision = function((paddle) {
+
+///////////////////////// FIX
+//NOTE We are supposed to write prototype not prototyp. Its a typo
+// There is one extra parenthese in paddle
+Ball.prototype.handleCollision = function(paddle) {
   // Check if the ball overlaps the paddle on x axis
   if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
     // Check if the ball overlaps the paddle on y axis
