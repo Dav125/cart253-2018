8dd506e0de15874a8565ede4ef281bf72e0a5a16
David Fong <dav770@outlook.com>
2018-11-13 16:37:20 -0500

    Ex 7: Downloaded all the necessary images and Setting up the item pages to write the code

---
+++ exercises/exercise7/js/Blender.js
@@ -0,0 +1,19 @@
+/*****************
+
+Blender
+By David Fong
+
+Player's avatar will be a
+blender
+******************/
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+function draw() {
+
+}
---
+++ exercises/exercise7/js/Strawberry.js
@@ -0,0 +1,20 @@
+/*****************
+
+Strawberry Fruit
+By David Fong
+
+Fruit to used to make a fruit smoothie or
+dropped to the floor
+
+******************/
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+function draw() {
+
+}
