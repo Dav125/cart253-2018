4f9b4c79dfcc0283d395c09e0c486dfc18ffb156
David Fong <dav770@outlook.com>
2018-11-13 22:02:10 -0500

    Ex7:  setup preload images and to display the fruits

--- exercises/exercise7/js/script.js
+++ exercises/exercise7/js/script.js
@@ -23,38 +23,123 @@ var kiwi;
 var banana;
 var strawberry;
 
+//variables for display the fruit
+var appleImage;
+var orangeImage;
+var kiwiImage;
+var bananaImage;
+var strawberryImage;
+
 // variables for the player's avatar which is it is
 // a blender
-
 var blender;
 
-function setup() {
-createCanvas(640,480);
 
-// Setting-up new fruits drop down
-// Apple
+// variable for the state of the game
+var state = "start";
 
+// Preload the images for fruit
+function preload() {
 
-// Orange
-
-
-// kiwi
-
-
-// banana
-
-
-// strawberry
-
-
-// Setting up the player avatar blender 
+appleImage = loadImage("assets/images/Apple.png");
+orangeImage = loadImage("assets/images/Orange.png");
+kiwiImage = loadImage("assets/images/kiwi.png");
+bananaImage = loadImage("assets/images/banana.png");
+strawberryImage = loadImage("assets/images/strawberry.png");
 
 
 }
 
-function draw(){
 
-// Code for the state of the game
+function setup() {
+  createCanvas(640, 480);
+
+  // Setting-up new fruits drop down
+  // Apple
+  apple = new Apple(random(0,width), height-600, 5, 50, 0.2);
+
+  // Orange
+  orange = new Orange (random(0,width), height-600, 7, 50, 0);
+
+  // kiwi
+  kiwi = new Kiwi (random(0,width), height-600, 4, 50, 0);
+
+
+  // banana
+  banana = new Banana (random(0,width), height-600, 2, 50, 0);
+
+  // strawberry
+  strawberry = new Strawberry (random(0,width), height-600, 6, 50, 0);
+
+  // Setting up the player avatar blender
+
+
+}
+
+// Start of the game
+//-------------------------
+// the start state of tthe game
+function startScreen() {
+
+  console.log("here is the title?") ;
+  textFont("Helvetica");
+  textSize(50);
+  textAlign(CENTER, CENTER);
+  noStroke();
+  fill(230, 10, 10);
+  text("Fruit Blender", width / 2, height / 4);
+
+  if (mouseIsPressed) {
+    state = "game";
+  }
+}
+
+function draw() {
+
+  background(100, 230, 200);
+  // Code for the state of the game
+  switch (state) {
+    case "start":
+      startScreen();
+      break;
+    case "game":
+      gameScreen();
+      break;
+    case "end":
+      gameOver();
+      break;
+  }
+}
+
+// the gameplay of the game
+// all the action of the actions are hapenning
+// here
+
+function gameScreen() {
+apple.display();
+banana.display();
+kiwi.display();
+orange.display();
+strawberry.display();
+
+apple.update();
+banana.update();
+kiwi.update();
+orange.update();
+strawberry.update();
+
+apple.isOffScreen();
+banana.isOffScreen();
+kiwi.isOffScreen();
+orange.isOffScreen();
+strawberry.isOffScreen();
+}
+
+
+
+// the game over screen is here
+
+function gameOver() {
 
 
 }
