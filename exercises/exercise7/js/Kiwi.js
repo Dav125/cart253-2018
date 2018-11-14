e7a8f83645312b735a2bfbdd16d9db0bc45cfa24
David Fong <dav770@outlook.com>
2018-11-13 22:00:34 -0500

    Setting up the Kiwi to be able to fall
    Copy and paste and replace

--- exercises/exercise7/js/Kiwi.js
+++ exercises/exercise7/js/Kiwi.js
@@ -11,10 +11,53 @@ dropped to the floor
 // setup()
 //
 // Description of setup
+// function of the Kiwi
+function Kiwi(x,y,vy,size,angle) {
 
-function setup() {
+  this.x = x;
+  this.y = y;
+  this.vy = vy;
+  this.size = size;
+  this.angle = angle;
 
 }
-function draw() {
+
+// update the postion of the fruit
+Kiwi.prototype.update = function () {
+// update the postion when it falls
+this.y = this.y + this.vy
+translate(this.x, this.y);
+rotate(this.angle);
+this.angle += 0.2;
+
+
+
+}
+
+// reset the postion of the x axis when it fall
+Kiwi.prototype.isOffScreen = function () {
+// the fruit reset its postion appearing on the
+// top
+
+if (this.y > height){
+this.y = height - 600;
+this.x = random(0,width);
+}
+}
+
+//display function
+//
+//Draw the Kiwi on the screen
+Kiwi.prototype.display = function() {
+imageMode(CENTER);
+image (kiwiImage, this.x, this.y, this.size, this.size);
+}
+
+
+// when the Kiwi collides with the blender you
+// score a point
+Kiwi.Prototype.handleCollision = function(blender){
+  // Check if the fruit collides with blender
+
 
 }
