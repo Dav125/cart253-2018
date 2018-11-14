5fe24899ff3cbe23236f86314a30e353fefd63d3
David Fong <dav770@outlook.com>
2018-11-13 21:59:08 -0500

    Ex7: Setting the item banana to fall
    copy and paste and replace the words

--- exercises/exercise7/js/Banana.js
+++ exercises/exercise7/js/Banana.js
@@ -8,13 +8,53 @@ dropped to the floor
 
 ******************/
 
-// setup()
-//
-// Description of setup
+// function of the banana
+function Banana(x,y,vy,size,angle) {
 
-function setup() {
+  this.x = x;
+  this.y = y;
+  this.vy = vy;
+  this.size = size;
+  this.angle = angle;
 
 }
-function draw() {
+
+// update the postion of the fruit
+Banana.prototype.update = function () {
+// update the postion when it falls
+this.y = this.y + this.vy
+translate(this.x, this.y);
+rotate(this.angle);
+this.angle += 0.2;
+
+
+
+}
+
+// reset the postion of the x axis when it fall
+Banana.prototype.isOffScreen = function () {
+// the fruit reset its postion appearing on the
+// top
+
+if (this.y > height){
+this.y = height - 600;
+this.x = random(0,width);
+}
+}
+
+//display function
+//
+//Draw the banana on the screen
+Banana.prototype.display = function() {
+imageMode(CENTER);
+image (bananaImage, this.x, this.y, this.size, this.size);
+}
+
+
+// when the banana collides with the blender you
+// score a point
+Banana.Prototype.handleCollision = function(blender){
+  // Check if the fruit collides with blender
+
 
 }
