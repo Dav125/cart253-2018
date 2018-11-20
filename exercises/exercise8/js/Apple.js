5903061e637402a80a5e73d5204db9e9325aefa3
David Fong <dav770@outlook.com>
2018-11-20 17:39:27 -0500

    ex8: made the apple shoot up

--- exercises/exercise8/js/Apple.js
+++ exercises/exercise8/js/Apple.js
@@ -22,9 +22,7 @@ function Apple(x,y,vy,size,angle) {
 // update the postion of the fruit
 Apple.prototype.update = function () {
 // update the postion when it falls
-this.y = this.y + this.vy
-translate(this.x, this.y);
-rotate(this.angle);
+this.y = this.y - this.vy   
 this.angle += 0.2;
 
 
@@ -48,19 +46,4 @@ this.x = random(0,width);
 Apple.prototype.display = function() {
 imageMode(CENTER);
 image (appleImage, this.x, this.y, this.size, this.size);
-}
-
-
-// when the apple collides with the blender you
-// score a point
-Apple.prototype.handleCollision = function(blender){
-  // Check if the fruit collides with blender
-if(this.x + this.size> blender.x && this.x < blender.x +blender.size){
-  // Check if the fruits overlaps with the blenderImage
-  if(this.x + this.size > blender.y && this.y < blender.y + blender.size){
-    this.y = height - 600;
-    this.x = random(0,width);
-  }
-}
-
 }
