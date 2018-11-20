9285c91215c3473edd0251fce18a26eeb41f407b
David Fong <dav770@outlook.com>
2018-11-20 17:48:36 -0500

    ex8: setup kiwi
    Same as the previous fruits

--- exercises/exercise8/js/Kiwi.js
+++ exercises/exercise8/js/Kiwi.js
@@ -1,6 +1,6 @@
 /*****************
 
-Apple Fruit
+Kiwi Fruit
 By David Fong
 
 Fruit to used to make a fruit smoothie or
@@ -8,8 +8,11 @@ dropped to the floor
 
 ******************/
 
-// function of the apple
-function Apple(x,y,vy,size,angle) {
+// setup()
+//
+// Description of setup
+// function of the Kiwi
+function Kiwi(x,y,vy,size,angle) {
 
   this.x = x;
   this.y = y;
@@ -20,11 +23,9 @@ function Apple(x,y,vy,size,angle) {
 }
 
 // update the postion of the fruit
-Apple.prototype.update = function () {
+Kiwi.prototype.update = function () {
 // update the postion when it falls
-this.y = this.y + this.vy
-translate(this.x, this.y);
-rotate(this.angle);
+this.y = this.y - this.vy
 this.angle += 0.2;
 
 
@@ -32,7 +33,7 @@ this.angle += 0.2;
 }
 
 // reset the postion of the x axis when it fall
-Apple.prototype.isOffScreen = function () {
+Kiwi.prototype.isOffScreen = function () {
 // the fruit reset its postion appearing on the
 // top
 
@@ -44,23 +45,8 @@ this.x = random(0,width);
 
 //display function
 //
-//Draw the apple on the screen
-Apple.prototype.display = function() {
+//Draw the Kiwi on the screen
+Kiwi.prototype.display = function() {
 imageMode(CENTER);
-image (appleImage, this.x, this.y, this.size, this.size);
-}
-
-
-// when the apple collides with the blender you
-// score a point
-Apple.prototype.handleCollision = function(blender){
-  // Check if the fruit collides with blender
-if(this.x + this.size> blender.x && this.x < blender.x +blender.size){
-  // Check if the fruits overlaps with the blenderImage
-  if(this.x + this.size > blender.y && this.y < blender.y + blender.size){
-    this.y = height - 600;
-    this.x = random(0,width);
-  }
-}
-
+image (kiwiImage, this.x, this.y, this.size, this.size);
 }
