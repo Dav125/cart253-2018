ec65cca3bf0051316ae543abdb491588cf6c7388
David Fong <dav770@outlook.com>
2018-11-20 17:50:58 -0500

    ex8: Setup strawberry
    same as previous fruits

--- exercises/exercise8/js/Strawberry.js
+++ exercises/exercise8/js/Strawberry.js
@@ -1,6 +1,6 @@
 /*****************
 
-Apple Fruit
+Strawberry Fruit
 By David Fong
 
 Fruit to used to make a fruit smoothie or
@@ -8,8 +8,11 @@ dropped to the floor
 
 ******************/
 
-// function of the apple
-function Apple(x,y,vy,size,angle) {
+// setup()
+//
+// Description of setup
+
+function Strawberry(x, y, vy, size, angle) {
 
   this.x = x;
   this.y = y;
@@ -20,47 +23,30 @@ function Apple(x,y,vy,size,angle) {
 }
 
 // update the postion of the fruit
-Apple.prototype.update = function () {
-// update the postion when it falls
-this.y = this.y + this.vy
-translate(this.x, this.y);
-rotate(this.angle);
-this.angle += 0.2;
+Strawberry.prototype.update = function() {
+  // update the postion when it falls
+  this.y = this.y - this.vy
+  this.angle += 0.2;
 
 
 
 }
 
 // reset the postion of the x axis when it fall
-Apple.prototype.isOffScreen = function () {
-// the fruit reset its postion appearing on the
-// top
+Strawberry.prototype.isOffScreen = function() {
+  // the fruit reset its postion appearing on the
+  // top
 
-if (this.y > height){
-this.y = height - 600;
-this.x = random(0,width);
-}
+  if (this.y > height) {
+    this.y = height - 600;
+    this.x = random(0, width);
+  }
 }
 
 //display function
 //
-//Draw the apple on the screen
-Apple.prototype.display = function() {
-imageMode(CENTER);
-image (appleImage, this.x, this.y, this.size, this.size);
-}
-
-
-// when the apple collides with the blender you
-// score a point
-Apple.prototype.handleCollision = function(blender){
-  // Check if the fruit collides with blender
-if(this.x + this.size> blender.x && this.x < blender.x +blender.size){
-  // Check if the fruits overlaps with the blenderImage
-  if(this.x + this.size > blender.y && this.y < blender.y + blender.size){
-    this.y = height - 600;
-    this.x = random(0,width);
-  }
-}
-
+//Draw the Strawberry on the screen
+Strawberry.prototype.display = function() {
+  imageMode(CENTER);
+  image(strawberryImage, this.x, this.y, this.size, this.size);
 }
