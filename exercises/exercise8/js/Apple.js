6a1a29dfce0e7015868dd1516c097c75a4ed2029
David Fong <dav770@outlook.com>
2018-11-20 17:45:04 -0500

    ex8: Used beautify here

--- exercises/exercise8/js/Apple.js
+++ exercises/exercise8/js/Apple.js
@@ -9,7 +9,7 @@ dropped to the floor
 ******************/
 
 // function of the apple
-function Apple(x,y,vy,size,angle) {
+function Apple(x, y, vy, size, angle) {
 
   this.x = x;
   this.y = y;
@@ -20,30 +20,30 @@ function Apple(x,y,vy,size,angle) {
 }
 
 // update the postion of the fruit
-Apple.prototype.update = function () {
-// update the postion when it falls
-this.y = this.y - this.vy   
-this.angle += 0.2;
+Apple.prototype.update = function() {
+  // update the postion when it falls
+  this.y = this.y - this.vy
+  this.angle += 0.2;
 
 
 
 }
 
 // reset the postion of the x axis when it fall
-Apple.prototype.isOffScreen = function () {
-// the fruit reset its postion appearing on the
-// top
+Apple.prototype.isOffScreen = function() {
+  // the fruit reset its postion appearing on the
+  // top
 
-if (this.y > height){
-this.y = height - 600;
-this.x = random(0,width);
-}
+  if (this.y > height) {
+    this.y = height - 600;
+    this.x = random(0, width);
+  }
 }
 
 //display function
 //
 //Draw the apple on the screen
 Apple.prototype.display = function() {
-imageMode(CENTER);
-image (appleImage, this.x, this.y, this.size, this.size);
+  imageMode(CENTER);
+  image(appleImage, this.x, this.y, this.size, this.size);
 }
